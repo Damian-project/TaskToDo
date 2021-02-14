fe8a05f3cbb7bee6f0cffdfb93197e4283ca7471
Damian <damian.bartnowski@gmail.com>
2021-02-14 16:31:52 +0100

    Initial Commit

---
+++ TaskToDo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TaskToDo
+//
+//  Created by PC on 14/02/2021.
+//  Copyright © 2021 db. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

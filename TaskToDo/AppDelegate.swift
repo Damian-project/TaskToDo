c4d7e28dba5d1920ad87993f123b66b90a9aad7b
Damian <damian.bartnowski@gmail.com>
2021-03-15 13:59:24 +0100

    Revert "Merge commit 'fc351498b1bdac38eb6f1b9f23dac4172fea7d30'"
    This reverts commit 0186ed713fadfe7b3974c57a6328417fb89a3ea0, reversing
    changes made to 1c5994ac48d2bfce49863ee5d82b0d31ad8a2663.

--- TaskToDo/AppDelegate.swift
+++ TaskToDo/AppDelegate.swift
@@ -22,10 +22,7 @@ var window: UIWindow?
     func applicationWillTerminate(_ application: UIApplication) {
         self.saveContext()
     }
-<<<<<<< .merge_file_ZhpQRV
     
-=======
->>>>>>> .merge_file_8qGmhB
   // MARK: - Core Data stack
 
      lazy var persistentContainer: NSPersistentCloudKitContainer = {

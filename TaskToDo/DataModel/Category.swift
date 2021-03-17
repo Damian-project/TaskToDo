9beb0d78726a8daaa589efc542d655f7f1153f65
Damian <damian.bartnowski@gmail.com>
2021-03-17 12:16:13 +0100

    commit Realm changes

---
+++ TaskToDo/DataModel/Category.swift
@@ -0,0 +1,15 @@
+//
+//  Category.swift
+//  TaskToDo
+//
+//  Created by PC on 16/03/2021.
+//  Copyright © 2021 db. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class Category: Object {
+    @objc dynamic var name : String = ""
+    let items = List<Item>()
+}
---
+++ TaskToDo/DataModel/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TaskToDo
+//
+//  Created by PC on 05/03/2021.
+//  Copyright © 2021 db. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
